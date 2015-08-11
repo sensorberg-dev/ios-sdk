bf584738ca4d10f8e58124cd7dc178950a96b8fc
tagyro <andrei.stoleru@gmail.com>
2015-08-11 18:28:31 +0200

    added enum for manager status

--- Pod/Classes/SBManager.h
+++ Pod/Classes/SBManager.h
@@ -10,13 +10,56 @@
 
 #import "SBAPIClient.h"
 #import "SBLocation.h"
+#import "SBBluetooth.h"
+
+/**
+ SBSDKManagerBackgroundAppRefreshStatus
+ 
+ Represents the app’s Background App Refresh status.
+ 
+ @since 0.7.0
+ */
+typedef NS_ENUM(NSInteger, SBSDKManagerBackgroundAppRefreshStatus) {
+    /**
+     Background App Refresh is enabled, the app is authorized to use location services and
+     Bluetooth is turned on.
+     */
+    SBSDKManagerBackgroundAppRefreshStatusAvailable,
+    
+    /**
+     This application is not enabled to use Background App Refresh. Due
+     to active restrictions on Background App Refresh, the user cannot change
+     this status, and may not have personally denied availability.
+     
+     Do not warn the user if the value of this property is set to
+     SBSDKManagerBackgroundAppRefreshStatusRestricted; a restricted user does not have
+     the ability to enable multitasking for the app.
+     */
+    SBSDKManagerBackgroundAppRefreshStatusRestricted,
+    
+    /**
+     User has explicitly disabled Background App Refresh for this application, or
+     Background App Refresh is disabled in Settings.
+     */
+    SBSDKManagerBackgroundAppRefreshStatusDenied,
+    
+    /**
+     This application runs on a device that does not support Background App Refresh.
+     */
+    SBSDKManagerBackgroundAppRefreshStatusUnavailable
+};
+
 
 @interface SBManager : NSObject {
     //
 }
 //
-@property (strong, nonatomic) SBAPIClient *apiClient;
-
+@property (strong, nonatomic) SBAPIClient   *apiClient;
+//
+@property (strong, nonatomic) SBLocation    *locClient;
+//
+@property (strong, nonatomic) SBBluetooth   *bleClient;
+//
 
 // 
 - (instancetype)init NS_UNAVAILABLE;
--- Pod/Classes/SBManager.m
+++ Pod/Classes/SBManager.m
@@ -11,7 +11,7 @@
 @implementation SBManager
 
 - (instancetype)init {
-    //should we throw an exception?
+    // throw an exception?
     return [self initWithResolver:@""
                            apiKey:@""];
 }
@@ -22,9 +22,108 @@
         //
         _apiClient = [[SBAPIClient alloc] initWithBaseURL:baseURL andAPI:apiKey];
         //
-        
+        _locClient = [[SBLocation alloc] init];
     }
     return self;
+}
+
+- (void)getLayout {
+    [_apiClient layout];
+    //
+}
+
+#pragma mark - Status
+
+//- (SBSDKManagerAvailabilityStatus)availabilityStatus {
+//    //
+//    switch (self.bluetoothStatus) {
+//        case SBSDKManagerBluetoothStatusPoweredOff:
+//            return SBSDKManagerAvailabilityStatusBluetoothRestricted;
+//            
+//        default:
+//            break;
+//    }
+//    
+//    switch (self.backgroundAppRefreshStatus) {
+//        case SBSDKManagerBackgroundAppRefreshStatusRestricted:
+//        case SBSDKManagerBackgroundAppRefreshStatusDenied:
+//            return SBSDKManagerAvailabilityStatusBackgroundAppRefreshRestricted;
+//            
+//        default:
+//            break;
+//    }
+//    
+//    switch (self.authorizationStatus) {
+//        case SBSDKManagerAuthorizationStatusNotDetermined:
+//        case SBSDKManagerAuthorizationStatusUnimplemented:
+//        case SBSDKManagerAuthorizationStatusRestricted:
+//        case SBSDKManagerAuthorizationStatusDenied:
+//            return SBSDKManagerAvailabilityStatusAuthorizationRestricted;
+//            
+//        default:
+//            break;
+//    }
+//    
+//    switch (self.connectionState) {
+//        case SBSDKManagerConnectionStateConnecting:
+//        case SBSDKManagerConnectionStateDisconnected:
+//            return SBSDKManagerAvailabilityStatusConnectionRestricted;
+//            
+//        default:
+//            break;
+//    }
+//    
+//    switch (self.reachabilityState) {
+//        case SBSDKManagerReachabilityStateNotReachable:
+//            return SBSDKManagerAvailabilityStatusReachabilityRestricted;
+//            
+//        default:
+//            break;
+//    }
+//    
+//    return SBSDKManagerAvailabilityStatusFullyFunctional;
+//}
+
+- (SBSDKManagerBackgroundAppRefreshStatus)backgroundAppRefreshStatus {
+    //
+    UIBackgroundRefreshStatus status = [UIApplication sharedApplication].backgroundRefreshStatus;
+    //
+    switch (status) {
+        case UIBackgroundRefreshStatusRestricted:
+            return SBSDKManagerBackgroundAppRefreshStatusRestricted;
+            
+        case UIBackgroundRefreshStatusDenied:
+            return SBSDKManagerBackgroundAppRefreshStatusDenied;
+            
+        case UIBackgroundRefreshStatusAvailable:
+            return SBSDKManagerBackgroundAppRefreshStatusAvailable;
+            
+        default:
+            break;
+    }
+    
+    return SBSDKManagerBackgroundAppRefreshStatusAvailable;
+}
+
+#pragma mark - SBAPIClient events
+
+SUBSCRIBE(SBELayout) {
+    if (event.error) {
+        // propagate error to delegate
+        return;
+    }
+    //
+    NSString *layoutCache = [event.layout toJSONString];
+    NSError *error;
+    BOOL cached = [layoutCache writeToFile:@"cacheFile" atomically:YES encoding:NSUTF8StringEncoding error:&error];
+    //
+    if (!cached) {
+        NSLog(@"failed to write layout cache");
+    }
+    //
+    if (self.locClient) {
+        
+    }
 }
 
 @end
