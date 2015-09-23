78779f00974f3b270ec38cc48cf38e007f7b64b6
tagyro <andrei.stoleru@gmail.com>
2015-09-23 11:16:14 +0200

    Added shake to reset in demo app

---
+++ Example/Sensorberg/SBTabBar.h
@@ -0,0 +1,13 @@
+//
+//  SBTabBar.h
+//  Sensorberg
+//
+//  Created by Andrei Stoleru on 22/09/15.
+//  Copyright © 2015 tagyro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface SBTabBar : UITabBarController
+
+@end
---
+++ Example/Sensorberg/SBTabBar.m
@@ -0,0 +1,37 @@
+//
+//  SBTabBar.m
+//  Sensorberg
+//
+//  Created by Andrei Stoleru on 22/09/15.
+//  Copyright © 2015 tagyro. All rights reserved.
+//
+
+#import "SBTabBar.h"
+
+@interface SBTabBar ()
+
+@end
+
+@implementation SBTabBar
+
+- (void)viewDidLoad {
+    [super viewDidLoad];
+    // Do any additional setup after loading the view.
+}
+
+- (void)didReceiveMemoryWarning {
+    [super didReceiveMemoryWarning];
+    // Dispose of any resources that can be recreated.
+}
+
+/*
+#pragma mark - Navigation
+
+// In a storyboard-based application, you will often want to do a little preparation before navigation
+- (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
+    // Get the new view controller using [segue destinationViewController].
+    // Pass the selected object to the new view controller.
+}
+*/
+
+@end
