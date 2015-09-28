160327ed800ec4e35e3fea090d1ba626ffb63c08
Andrei Stoleru <tagyro@users.noreply.github.com>
2015-09-28 21:26:30 +0200

    Update SBSDKManager.m

--- SensorbergSDK/SBSDKManager.m
+++ SensorbergSDK/SBSDKManager.m
@@ -38,7 +38,7 @@
 #import "SBSDKDeviceID.h"
 #import "SBSDKMacros.h"
 
-#define OS_VERSION [[UIDevice currentDevice].systemVersion componentsSeparatedByString:@"."][0].integerValue
+#define OS_VERSION [[[UIDevice currentDevice].systemVersion componentsSeparatedByString:@"."] firstObject].integerValue
 
 #pragma mark -
 
