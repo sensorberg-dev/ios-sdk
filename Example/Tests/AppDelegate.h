3184352b8cba7e18f864ed704702ac412387e267
tagyro <andrei.stoleru@gmail.com>
2015-10-19 13:44:51 +0200

    Updated testing

---
+++ Example/Tests/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  Tests
+//
+//  Created by Andrei Stoleru on 19/10/15.
+//  Copyright © 2015 tagyro. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
