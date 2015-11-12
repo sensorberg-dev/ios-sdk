45f6bf667a38584107f7c2cb6d6bc7094224b9c8
tagyro <andrei.stoleru@gmail.com>
2015-11-12 18:14:53 +0100

    Added SBSettings for remotely controlled settings

---
+++ Pod/Classes/SBSettings.h
@@ -0,0 +1,13 @@
+//
+//  SBSettings.h
+//  Pods
+//
+//  Created by andsto on 12/11/15.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SBSettings : NSObject
+
+@end
---
+++ Pod/Classes/SBSettings.m
@@ -0,0 +1,13 @@
+//
+//  SBSettings.m
+//  Pods
+//
+//  Created by andsto on 12/11/15.
+//
+//
+
+#import "SBSettings.h"
+
+@implementation SBSettings
+
+@end
