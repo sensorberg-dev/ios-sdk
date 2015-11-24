9a182f60d8457719afd0e058b527fcf097e0d4f2
tagyro <andrei.stoleru@gmail.com>
2015-11-24 14:49:13 +0100

    Fixed import

--- Pod/Classes/SBAnalytics.m
+++ Pod/Classes/SBAnalytics.m
@@ -22,7 +22,7 @@
 //  OUT OF OR IN CONNECTION WITH THE SOFTWARE OR THE USE OR OTHER DEALINGS IN
 //  THE SOFTWARE.
 //
-#import <Tolo/tolo.h>
+#import <tolo/Tolo.h>
 
 #import "SBAnalytics.h"
 
--- Pod/Classes/SensorbergSDK.h
+++ Pod/Classes/SensorbergSDK.h
@@ -33,7 +33,7 @@ FOUNDATION_EXPORT const unsigned char SensorbergSDKVersionString[];
 
 // In this header, you should import all the public headers of your framework using statements like #import <SensorbergSDK/PublicHeader.h>
 
-#import <Tolo/tolo.h>
+#import <tolo/Tolo.h>
 
 #import <JSONModel/JSONModel.h>
 
