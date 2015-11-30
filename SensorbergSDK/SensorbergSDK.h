cc4da4d52146a5376c51824ac8a91bcc7b876794
Andrei <andsto@airsb.local>
2015-11-30 10:31:55 +0100

    Initial Commit

---
+++ SensorbergSDK/SensorbergSDK.h
@@ -0,0 +1,13 @@
+//
+//  SensorbergSDK.h
+//  SensorbergSDK
+//
+//  Created by andsto on 30/11/15.
+//  Copyright © 2015 Sensorberg GmbH. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+@interface SensorbergSDK : NSObject
+
+@end
---
+++ SensorbergSDK/SensorbergSDK.m
@@ -0,0 +1,13 @@
+//
+//  SensorbergSDK.m
+//  SensorbergSDK
+//
+//  Created by andsto on 30/11/15.
+//  Copyright © 2015 Sensorberg GmbH. All rights reserved.
+//
+
+#import "SensorbergSDK.h"
+
+@implementation SensorbergSDK
+
+@end
