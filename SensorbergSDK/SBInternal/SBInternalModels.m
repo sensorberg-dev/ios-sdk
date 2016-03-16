1066b155e71df7ebd0561ebc87ee9fe7b363b25d
andsto <andrei.stoleru@sensorberg.com>
2016-03-16 14:46:19 +0100

    Cleaner event publishing

--- SensorbergSDK/SBInternal/SBInternalModels.m
+++ SensorbergSDK/SBInternal/SBInternalModels.m
@@ -111,11 +111,10 @@
                         SBLog(@"🔔 Campaign \"%@\"",campaignAction.subject);
                         [keychain setString:[dateFormatter stringFromDate:now] forKey:action.eid];
                         //
-                        PUBLISH((({
-                            SBEventPerformAction *event = [SBEventPerformAction new];
-                            event.campaign = campaignAction;
-                            event;
-                        })));
+                        SBEventPerformAction *event = [SBEventPerformAction new];
+                        event.campaign = campaignAction;
+                        //
+                        PUBLISH(event);
                         //
                         if (action.reportImmediately) {
                             PUBLISH([SBEventReportHistory new]);
