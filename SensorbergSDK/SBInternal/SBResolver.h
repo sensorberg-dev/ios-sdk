3d0b1d83aa18552f6ee926e882b107ee83ec11d2
Park Sanggeon <gunnih@gmail.com>
2016-11-17 15:26:22 +0100

    removed compile time warning.

--- SensorbergSDK/SBInternal/SBResolver.h
+++ SensorbergSDK/SBInternal/SBResolver.h
@@ -32,15 +32,15 @@
 
 @interface SBResolver : NSObject
 
-- (instancetype)initWithApiKey:(NSString*)key;
+- (nonnull instancetype)initWithApiKey:(NSString * _Nonnull)key;
 
 - (BOOL)isConnected;
 
 - (void)ping;
 
-- (void)requestLayoutForBeacon:(SBMBeacon*)beacon trigger:(SBTriggerType)trigger useCache:(BOOL)useCache;
+- (void)requestLayoutForBeacon:(SBMBeacon * _Nullable)beacon trigger:(SBTriggerType)trigger useCache:(BOOL)useCache;
 
-- (void)postLayout:(SBMPostLayout*)postData __attribute__((nonnull));
+- (void)postLayout:(SBMPostLayout * _Nonnull)postData __attribute__((nonnull));
 
 - (void)requestSettingsWithAPIKey:(NSString * _Nonnull)key;
 
