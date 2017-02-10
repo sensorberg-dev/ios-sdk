78093c6af9c727a0ed5d29afbf6b65c96ee70bb8
andsto <andrei.stoleru@sensorberg.com>
2017-02-10 16:08:06 +0100

    Added SBMagnetometer start/stop monitoring on app active/inactive

---
+++ SensorbergSDK/SBMagnetometer.h
@@ -0,0 +1,19 @@
+//
+//  SBMagnetometer.h
+//  Pods
+//
+//  Created by Andrei Stoleru on 09/02/17.
+//
+//
+
+#import <Foundation/Foundation.h>
+
+#import <CoreMotion/CoreMotion.h>
+
+#import <tolo/Tolo.h>
+
+@interface SBMagnetometer : NSObject
+
+- (void)startMonitoring;
+
+@end
---
+++ SensorbergSDK/SBMagnetometer.m
@@ -0,0 +1,79 @@
+//
+//  SBMagnetometer.m
+//  Pods
+//
+//  Created by Andrei Stoleru on 09/02/17.
+//
+//
+
+#import "SBMagnetometer.h"
+
+#import "SBEvent.h"
+
+@interface SBMagnetometer () {
+    CMMotionManager *motionManager;
+    
+    NSOperationQueue *queue;
+}
+@end
+
+@implementation SBMagnetometer
+
+#pragma mark - SBMagnetometer
+
+static SBMagnetometer * _sharedManager;
+
+static dispatch_once_t once;
+
++ (instancetype)sharedManager {
+    if (!_sharedManager) {
+        //
+        dispatch_once(&once, ^ {
+            _sharedManager = [[self alloc] init];
+        });
+        //
+    }
+    return _sharedManager;
+}
+
+- (instancetype)init
+{
+    self = [super init];
+    if (self) {
+        queue = [NSOperationQueue new];
+        queue.qualityOfService = NSQualityOfServiceUserInitiated;
+    }
+    return self;
+}
+
+#pragma mark - 
+
+- (void)startMonitoring {
+    if (!motionManager) {
+        motionManager = [[CMMotionManager alloc] init];
+    }
+    //
+    [motionManager startMagnetometerUpdatesToQueue:queue
+                                       withHandler:^(CMMagnetometerData * _Nullable magnetometerData, NSError * _Nullable error) {
+                                           NSLog(@"x: %d",magnetometerData.magneticField.x);
+                                           NSLog(@"y: %d",magnetometerData.magneticField.x);
+                                           NSLog(@"z: %d",magnetometerData.magneticField.x);
+                                       }];
+    //
+}
+
+- (void)stopMonitoring {
+    [motionManager stopMagnetometerUpdates];
+}
+
+#pragma mark - Events
+
+SUBSCRIBE(SBEventApplicationActive) {
+    [self startMonitoring];
+}
+
+SUBSCRIBE(SBEventApplicationWillResignActive) {
+    [self stopMonitoring];
+}
+
+@end
