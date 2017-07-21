29efd418ca2e72b447bf12bf7e403dbad04f3288
Andrei Stoleru <andrei.stoleru@sensorberg.com>
2017-07-21 15:11:47 +0200

    Minor cleanup

--- SBDemoApp/BeaconsViewController.m
+++ SBDemoApp/BeaconsViewController.m
@@ -107,13 +107,12 @@ SUBSCRIBE(SBEventLocationAuthorization) {
 
 #pragma mark SBEventBluetoothAuthorization
 SUBSCRIBE(SBEventBluetoothAuthorization) {
-    
+    // You only need to ask for Bluetooth authorisation when working with GATT
 }
 
 SUBSCRIBE(SBEventNotificationsAuthorization) {
     if (!event.notificationsAuthorization) {
-//        When notifications are not allowed 
-//        [[SBManager sharedManager] stopMonitoring];
+//        When notifications are not allowed
     }
 }
 
@@ -127,7 +126,6 @@ SUBSCRIBE(SBEventRegionEnter) {
 #pragma mark SBEventRegionExit
 SUBSCRIBE(SBEventRegionExit) {
     [beacons setValue:nil forKey:event.beacon.tid];
-    //    NSLog(@"Exit region: %@ (M:%i m:%i)", [NSString hyphenateUUIDString:event.beacon.uuid], event.beacon.major, event.beacon.minor);
     [self.tableView reloadData];
 }
 
@@ -157,61 +155,7 @@ SUBSCRIBE(SBEventPerformAction) {
     //
 }
 
-#pragma mark -
-
-/*
- - (UITableViewCell *)tableView:(UITableView *)tableView cellForRowAtIndexPath:(NSIndexPath *)indexPath {
- UITableViewCell *cell = [tableView dequeueReusableCellWithIdentifier:@"reuseIdentifier" forIndexPath:indexPath];
- 
- // Configure the cell...
- 
- return cell;
- }
- */
-
-/*
- // Override to support conditional editing of the table view.
- - (BOOL)tableView:(UITableView *)tableView canEditRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the specified item to be editable.
- return YES;
- }
- */
-
-/*
- // Override to support editing the table view.
- - (void)tableView:(UITableView *)tableView commitEditingStyle:(UITableViewCellEditingStyle)editingStyle forRowAtIndexPath:(NSIndexPath *)indexPath {
- if (editingStyle == UITableViewCellEditingStyleDelete) {
- // Delete the row from the data source
- [tableView deleteRowsAtIndexPaths:@[indexPath] withRowAnimation:UITableViewRowAnimationFade];
- } else if (editingStyle == UITableViewCellEditingStyleInsert) {
- // Create a new instance of the appropriate class, insert it into the array, and add a new row to the table view
- }   
- }
- */
-
-/*
- // Override to support rearranging the table view.
- - (void)tableView:(UITableView *)tableView moveRowAtIndexPath:(NSIndexPath *)fromIndexPath toIndexPath:(NSIndexPath *)toIndexPath {
- }
- */
-
-/*
- // Override to support conditional rearranging of the table view.
- - (BOOL)tableView:(UITableView *)tableView canMoveRowAtIndexPath:(NSIndexPath *)indexPath {
- // Return NO if you do not want the item to be re-orderable.
- return YES;
- }
- */
-
-/*
- #pragma mark - Navigation
- 
- // In a storyboard-based application, you will often want to do a little preparation before navigation
- - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender {
- // Get the new view controller using [segue destinationViewController].
- // Pass the selected object to the new view controller.
- }
- */
+#pragma mark - Utility
 
 -(UIImage *)imageFromText:(NSString *)text
 {
